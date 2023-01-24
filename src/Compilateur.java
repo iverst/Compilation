5b4e20b8b790541c901695d5f3becb0e676ebfc3
Arthur Chevalier <arthur.a.chevalier@gmail.com>
2023-01-24 15:35:34 +0100

    Creation differentes classes et lecture du fichier

--- src/Compilateur.java
+++ src/Compilateur.java
@@ -1,8 +1,11 @@
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Compilateur {
     public static final int LONG_MAX_IDENT = 20;
     public static final int LONG_MAX_CHAINE = 50;
     public static final int NB_MOTS_RESERVES = 7;
-    public static String SOURCE = "test.txt";
+    public static String SOURCE = "code/test.txt";
     public static char CARLU;
     public static int NOMBRE;
     public static String CHAINE;
@@ -12,7 +15,22 @@ public class Compilateur {
     public void compiler(){
         AnalyseurLexical analyseurLexical = new AnalyseurLexical(this);
         analyseurLexical.analyser();
-
+        lireFichier(SOURCE);
     }
 
+    public String lireFichier(String filePath) {
+        String data = "";
+        try {
+            Scanner scanner = new Scanner(new FileReader(filePath));
+            while (scanner.hasNextLine()) {
+                data += scanner.nextLine() + "\n";
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(data);
+        return data;
+
+    }
 }
