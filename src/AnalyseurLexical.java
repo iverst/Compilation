1b24d5f4deb27df84f2110a74fdc31675d0a2d5f
Arthur Chevalier <arthur.a.chevalier@gmail.com>
2023-02-07 16:52:11 +0100

    Ajout fonction SAUTER_SEPARATEURS commentaire imbrique

--- src/AnalyseurLexical.java
+++ src/AnalyseurLexical.java
@@ -10,6 +10,20 @@ import java.util.Scanner;
 public class AnalyseurLexical {
     private Compilateur compilateur;
     private char[] chars;
+    //nombre entiers
+    private final int MAX_INT = 32767;
+    private char[] chiffres = "0123456789".toCharArray();
+    //chaines charactères
+    private final int MAX_STRING = 50;
+    //identificateurs
+    private final int MAX_MOTCLE = 20;
+    private String[] motsClés = {"DEBUT", "FIN", "VAR", "ECRIRE", "LIRE","SI"};
+    //symboles simples
+    private final char[] symbolesSimples = ",;.:()<>=+*-/".toCharArray();
+    //symboles composes
+    private final String[] symbolesComposes = {"<=", ">=", "<>", ":="};
+
+
     public AnalyseurLexical(Compilateur compilateur) {
         this.compilateur = compilateur;
     }
@@ -39,26 +53,40 @@ public class AnalyseurLexical {
     }
     ////////////////////////////////////////////////////////////////////////////////////////////////////
     private void SAUTER_SEPARATEURS() {
+        //tabulation
         if (Compilateur.CARLU == ' ' || Compilateur.CARLU == '\t') {
             while (Compilateur.CARLU == ' ' || Compilateur.CARLU == '\t') {
-                System.out.println("Esapce :" + "'" + Compilateur.CARLU + "',");
                 LIRE_CHAR();
             }
         }
+        //commentaire
         if (Compilateur.CARLU == '{') {
-            LIRE_CHAR();
             System.out.println("Commentaire :" + "'" + Compilateur.CARLU + "',");
-            while (Compilateur.CARLU != '{' && Compilateur.CARLU != '}' ) {
-                System.out.println("Commentaire :" + "'" + Compilateur.CARLU + "',");
 
+            LIRE_CHAR();
+            int nbCommentaire = 1;
+
+            while (nbCommentaire != 0 ) {
+                System.out.println(Compilateur.CARLU + "  :  " + String.valueOf(nbCommentaire));
+                if (Compilateur.CARLU == '{') {
+                    nbCommentaire++;
+                }
+                else if (Compilateur.CARLU == '}') {
+                    nbCommentaire--;
+                }
                 LIRE_CHAR();
+
             }
         }
     }
 
     public T_UNILEX RECO_ENTIER() {
-        char[] chiffres = "0123456789".toCharArray();
-
+        String nombre = "";
+        while (Tools.getIntance().contains(chiffres ,Compilateur.CARLU)) {
+            nombre += Compilateur.CARLU;
+            LIRE_CHAR();
+        }
+        //if (nombre.length() > Compilateur.M)
         return null;
     }
 
